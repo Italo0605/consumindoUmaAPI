ab1312615912cad1e5321cf47ecf054cd54af3bb
Italo0605 <italosatiro2@gmail.com>
2022-05-24 16:41:07 -0300

    primeiro import

---
+++ ExemploRefit/ICepApiService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Refit;
+namespace ExemploRefit
+{
+    public interface ICepApiService
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepResponse> GetAddresAsync(string cep);
+    }
+}
+ 
---
+++ ExemploRefit/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Refit;
+using System.Threading.Tasks;
+
+namespace ExemploRefit 
+{
+    class MainClass
+    {
+         static async Task Main(string[] args)
+         {
+            try
+            {
+                var cepClient = RestService.For<ICepApiService>("https://viacep.com.br");
+                Console.WriteLine("Informe seu CEP:");
+
+                string cepInformado = Console.ReadLine().ToString();
+                Console.WriteLine("Consutando informações do CEP: " + cepInformado);
+
+                var address = await cepClient.GetAddresAsync(cepInformado);
+
+                Console.WriteLine($"\nLogradouro:{address.Logradouro}, \nBairro: {address.Bairro}, \nCidade: {address.Localidade} ");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro na consulta de CEP: " + e.Message);
+
+            }
+        }
+    }
+
+
+}
